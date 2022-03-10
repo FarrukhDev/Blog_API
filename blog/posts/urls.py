d2fe7b2c50e212dffe889b306f1744fe6ac4f43d
Farrux <farruxbekergashaliev@gmail.com>
2022-03-10 22:58:14 +0500

    last init

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, PostDelete, PostPut, PostPatch, PostPost
 
 urlpatterns = [
     path('<int:pk>/', PostDetail.as_view()),
     path('', PostList.as_view()),
+    path('delete/<int:pk>',PostDelete.as_view()),
+    path('put/<int:pk>',PostPut.as_view()),
+    path('patch/<int:pk>',PostPatch.as_view()),
+    path('post/',PostPost.as_view()),
 
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -2,12 +2,35 @@ from rest_framework import generics
 from .models import Post
 from .serializer import PostSerializer
 from .permissions import IsAuthorOrReadOnly
+from rest_framework.filters import OrderingFilter, SearchFilter
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    ordering_fields = ["__all__"]
+    search_fields = ["__all__"]
+
+class PostPost(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    ordering_fields = ["__all__"]
+    search_fields = ["__all__"]
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    ordering_fields = ["__all__"]
+    search_fields = ["__all__"]
+
+class PostDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostPut(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostPatch(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
